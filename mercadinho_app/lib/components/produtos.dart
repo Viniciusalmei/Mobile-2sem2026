5f953ae693a036f904645bd6ef305b82ba8c3b9d
Viniciusalmei <vihhalmeida12@gmail.com>
2026-09-21 11:21:18 -0300

    Começando carrinho

--- mercadinho_app/lib/components/produtos.dart
+++ mercadinho_app/lib/components/produtos.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mercadinho_app/screens/telacarrinho.dart';
 
 class ProdutosCard extends StatelessWidget {
   String nome; 
@@ -13,7 +14,10 @@ class ProdutosCard extends StatelessWidget {
         children: [
           Image.network(urlImagem,width: 100,height: 100,),
           Text(nome,style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
-          Text("R\$ ${preco.toString()}")
+          Text("R\$ ${preco.toString()}"),
+          IconButton(onPressed:(){
+           produtosCarrinho.add(ProdutosCard(nome: nome, preco: preco, urlImagem: urlImagem));
+          }, icon: Icon(Icons.add))
         ],
       )
     );
